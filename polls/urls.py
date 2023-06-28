2e4cfc6037f18286154f4ede755a07ae59362fc7
Vladimir <htreag@gmail.com>
2023-06-28 10:03:05 +0300

    hm10_completed

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,7 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('triangle/', views.triangle, name='triangle'),
+    path('triangle', views.triangle, name='triangle'),
+    path('person/', views.create_person, name='create_person'),
+    path('person/<int:id>', views.edit_person, name='edit_person'),
 ]
