027dc576d70611380b99aed1636c704e749f5a47
Vladimir <htreag@gmail.com>
2023-07-10 20:16:09 +0300

    hm12_completed

--- shop/urls.py
+++ shop/urls.py
@@ -6,8 +6,14 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
     path('carshop', views.carshop, name='carshop'),
-    path('engines', views.engines, name='engines'),
+    path('carshop/<int:carshop_id>/', views.carshop_detail, name='carshop_detail'),
+    path('engine_list', views.engine_list, name='engine_list'),
+    path('engine/<int:engine_id>/', views.engine_detail, name='engine_detail'),
     path('car_list', views.car_list, name='car_list'),
     path('car/<int:car_id>/', views.car_detail, name='car_detail'),
-    path('manufacturercountry', views.manufacturercountry, name='manufacturercountry'),
+    path('manufacturercountry_list', views.manufacturercountry_list, name='manufacturercountry_list'),
+    path('manufacturercountry/<int:manufacturercountry_id>/', views.manufacturercountry_detail, name='manufacturercountry_detail'),
+    path('color_list', views.color_list, name='color_list'),
+    path('color/<int:color_id>/', views.color_detail, name='color_detail'),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Car
+from .models import Car, CarShop, Engine, ManufacturerCountry, Color
+from django.core.paginator import Paginator
+from django.db.models import Case, When, Value, CharField, Count, Sum
+
+
 
 
 def index(request):
@@ -7,26 +11,114 @@ def index(request):
 
 
 def carshop(request):
-    return render(request, 'shop/carshop.html')
+    shop_list = CarShop.objects.all()
+    paginator = Paginator(shop_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'shop_list': page_obj
+    }
+    return render(request, 'shop/carshop.html', context=context)
 
 
-def engines(request):
-    return render(request, 'shop/engines.html')
+def carshop_detail(request, carshop_id):
+    carshop = get_object_or_404(CarShop, id=carshop_id)
+    carshop_prefetched = CarShop.objects.select_related('car').annotate(
+        total_count=Sum('count')
+    ).get(id=carshop_id)
+
+    context = {
+        'carshop': carshop_prefetched
+    }
+    return render(request, 'shop/carshop_detail.html', context=context)
+
+def engine_list(request):
+    engine_list = Engine.objects.all()
+    paginator = Paginator(engine_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'engine_list': page_obj
+    }
+    return render(request, 'shop/engine_list.html', context=context)
+
+
+def engine_detail(request, engine_id):
+    engine = get_object_or_404(Engine, id=engine_id)
+    manufacturercountry = ManufacturerCountry.objects.filter(engine=engine).first()
+    engine = engine.__class__.objects.annotate(
+        power_range=Case(
+            When(power__lte=100, then=Value('Low')),
+            When(power__gt=100, power__lte=200, then=Value('Medium')),
+            When(power__gt=200, then=Value('High')),
+            default=Value('Unknown'),
+            output_field=CharField(),
+        )
+    ).get(id=engine_id)
+    context = {
+        'engine': engine,
+        'manufacturercountry': manufacturercountry
+    }
+    return render(request, 'shop/engine_detail.html', context=context)
+
 
 
 def car_list(request):
     cars_list = Car.objects.all()
+    paginator = Paginator(cars_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'cars_list': cars_list
+        'cars_list': page_obj
     }
     return render(request, 'shop/car_list.html', context=context)
 
 def car_detail(request, car_id):
     car = get_object_or_404(Car, id=car_id)
+    car_prefetched = Car.objects.prefetch_related(
+        'engine',
+        'transmission',
+        'color',
+        'carshop_set'
+    ).annotate(
+        total_count=Count('carshop')
+    ).get(id=car_id)
+
     context = {
-        'car': car
+        'car': car_prefetched
     }
     return render(request, 'shop/car_detail.html', context=context)
 
-def manufacturercountry(request):
-    return render(request, 'shop/manufacturercountry.html')
+def manufacturercountry_list(request):
+    manufacturercountry_list = ManufacturerCountry.objects.all()
+    paginator = Paginator(manufacturercountry_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'manufacturercountry_list': page_obj
+    }
+    return render(request, 'shop/manufacturercountry_list.html', context=context)
+
+def manufacturercountry_detail(request, manufacturercountry_id):
+    manufacturercountry = get_object_or_404(ManufacturerCountry, id=manufacturercountry_id)
+    context = {
+        'manufacturercountry': manufacturercountry
+    }
+    return render(request, 'shop/manufacturercountry_detail.html', context=context)
+
+def color_list(request):
+    color_list = Color.objects.all()
+    paginator = Paginator(color_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'color_list': page_obj
+    }
+    return render(request, 'shop/color_list.html', context=context)
+
+def color_detail(request, color_id):
+    color = get_object_or_404(Color, id=color_id)
+    context = {
+        'color': color
+    }
+    return render(request, 'shop/color_detail.html', context=context)
